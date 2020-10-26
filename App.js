02a70a38671c2304d1582b51b7a13bc0c2fafb9b
nicholasgalante1997 <nicholasgalante1997@gmail.com>
2020-10-26 13:25:14 -0400

    fonts loaded

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import {AppLoading} from 'expo'
+import * as Font from 'expo-font'
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "tommy-bold": require('./src/constants/fonts/Made_Tommy_Bold.otf')
+  })
+}
+
+
 export default function App() {
+
+  const [fontLoaded, setFontLoaded] = useState(false)
+
+  if (!fontLoaded) {
+    return <AppLoading 
+    startAsync={fetchFonts}
+    onFinish={() => setFontLoaded(true)}
+    onError={err => console.log(err)} 
+    />
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.styledText}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +39,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  styledText: {
+    fontFamily: 'tommy-bold'
+  }
 });
---
+++ src/screens/LandingScreen.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {View, StyleSheet} from 'react-native'
+
+const LandingScreen = () => {
+    return ( 
+        <View>
+            
+        </View>
+     );
+}
+ 
+
+const styles = StyleSheet.create({
+
+})
+
+export default LandingScreen;
